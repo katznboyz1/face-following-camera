0e72e003aa4058387dbaf84520e70d39705eeb99
katznboyz <38505228+katznboyz1@users.noreply.github.com>
2019-09-18 13:19:49 -0400

    Added new command line arguments

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import pygame, cv2, sys, os, time
 
 #set that it is not in pitft mode
 PITFTMode = False
-#if the argument "-pitft" is supplied then switch to pitft mode
+#if the argument "--pitft" is supplied then switch to pitft mode
 if ('--pitft' in sys.argv):
     #pitft mode is where it sets up the screen and environment for running on a pitft screen, dont use the "-pitft" option for use on a normal display
     PITFTMode = True
+
+#set that it is not in graphical mode
+graphicalMode = False
+#if the argument "--gui" is supplied then switch to graphical mode
+if ('--gui' in sys.argv):
+    graphicalMode = True
+
+#set that large image mode is false
+largeImageMode = False
+#if the argument "--large-image" is supplied then switch to large image mode
+if ('--large-image' in sys.argv):
+    largeImageMode = True
 
 if (PITFTMode):
     initializeCommands = [
@@ -30,80 +42,84 @@ if (PITFTMode):
 camera = cv2.VideoCapture(0)
 cameraViewPath = './camera-view.jpg'
 
-'''
-#set the camera dimensions to 1024x768
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
-'''
 
-#initialize pygame
-pygame.display.init()
+#set the camera dimensions to 1024x768 if large image mode is on
+if (largeImageMode):
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
 
-#get the size of the screen
-screenSize = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 
-#initialize the pygame screen and clock
-screen = pygame.display.set_mode(screenSize, pygame.FULLSCREEN)
-clock = pygame.time.Clock()
-
-#set up the variables to be used for the draw loop
+#create a variable to control the while loop
 running = True
-wantedFPS = 30
-pygame.mouse.set_visible(False)
+
+#set up pygame stuff if graphical mode is in
+if (graphicalMode):
+    #initialize pygame
+    pygame.display.init()
+
+    #get the size of the screen
+    screenSize = [pygame.display.Info().current_w, pygame.display.Info().current_h]
+
+    #initialize the pygame screen and clock
+    screen = pygame.display.set_mode(screenSize, pygame.FULLSCREEN)
+    clock = pygame.time.Clock()
+
+    #set up the variables to be used for the draw loop
+    wantedFPS = 30
+    pygame.mouse.set_visible(False)
 
 #print out the system information
 print('''
 INFO:
 The program {} running in PITFT mode. (--pitft)
-The screen resolution is {}.
-Press the escape key to exit.
-'''.format(str('is' if PITFTMode else 'isnt'), screenSize))
+The program {} running in graphical mode. (--gui){}
+'''.format(str('is' if PITFTMode else 'isnt'), str('is' if graphicalMode else 'isnt'), str('\nPress the escape key to exit.' if graphicalMode else '')))
 
 #set up the face cascade
 currentPath = os.getcwd()
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#go through the main draw loop if the program is still supposed to be running
+#go through the main while loop if the program is still supposed to be running
 while (running):
-    #fill the background as solid white
-    screen.fill([255, 255, 255])
+    #fill the background as solid black if graphical mode is on
+    if (graphicalMode):
+        screen.fill([0, 0, 0])
 
     #if the camera image is available then show it on screen
     status, image = camera.read()
     if (status):
-        #set the screen brightness to full
-        os.system('gpio -g pwm 18 {}'.format(1000))
-
         #write the cameras view to the current camera view path
         cv2.imwrite(cameraViewPath, image)
 
         #do facial recognition stuff and save final image
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 3, minSize = (20, 20))
+        faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 3, minSize = (35, 35))
         for [x, y, w, h] in faces:
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.imwrite(cameraViewPath, image)
-        print (faces)
 
-        #draw the image to the screen and center it
-        cameraViewImagePygame = pygame.image.load(cameraViewPath)
-        imageSize = cameraViewImagePygame.get_size()
-        if (imageSize[0] > screenSize[0]):
-            aspectRatio = imageSize[1] / imageSize[0]
-            imageSize = [screenSize[0], int(screenSize[0] * aspectRatio)]
-            cameraViewImagePygame = pygame.transform.scale(cameraViewImagePygame, imageSize)
-        imageCoords = [int((screenSize[0] - imageSize[0]) / 2), int((screenSize[1] - imageSize[1]) / 2)]
-        screen.blit(cameraViewImagePygame, imageCoords)
+        #draw the image to the screen and center it if graphical mode is on
+        if (graphicalMode):
+            cameraViewImagePygame = pygame.image.load(cameraViewPath)
+            imageSize = cameraViewImagePygame.get_size()
+            if (imageSize[0] > screenSize[0]):
+                aspectRatio = imageSize[1] / imageSize[0]
+                imageSize = [screenSize[0], int(screenSize[0] * aspectRatio)]
+                cameraViewImagePygame = pygame.transform.scale(cameraViewImagePygame, imageSize)
+            imageCoords = [int((screenSize[0] - imageSize[0]) / 2), int((screenSize[1] - imageSize[1]) / 2)]
+            screen.blit(cameraViewImagePygame, imageCoords)
 
-    #get the keydown events
-    events = pygame.event.get()
+    #if it is in graphical mode then do graphical stuff
+    if (graphicalMode):
+        #get the keydown events
+        events = pygame.event.get()
 
-    #check if the escape key has been pressed, and if it has then close the window
-    for event in events:
-        if (event.type == pygame.KEYDOWN):
-            if (event.key == pygame.K_ESCAPE):
-                running = False
+        #check if the escape key has been pressed, and if it has then close the window
+        for event in events:
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_ESCAPE):
+                    running = False
 
-    #update the screen and set the fps
-    pygame.display.update()
-    clock.tick(wantedFPS)
+        #update the screen and set the fps
+        pygame.display.update()
+        clock.tick(wantedFPS)
