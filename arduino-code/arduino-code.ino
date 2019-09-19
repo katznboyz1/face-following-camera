b6f5475f19f7ca553b0ea90aa5bb119af160c5b7
katznboyz <38505228+katznboyz1@users.noreply.github.com>
2019-09-19 18:05:57 -0400

    Fixed a servo bounds issue
    Previously the servo could try to go over its bounds but now it cant

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -19,15 +19,20 @@ int reading; //the reading that the serial will give
 void setup() { //setup function
     Serial.begin(9600); //begin serial communication on 9600 baud
     while (!Serial) {;} //wait for serial communication to begin
+    
     SERVO_X.attach(SERVO_X_PIN); //attach the yaw controller servo to the designated pin
     SERVO_Y.attach(SERVO_Y_PIN); //attach the pitch controller servo to the designated pin
+    
     pinMode(LED_BUILTIN, OUTPUT); //set the pinmode for the builtin led
 }
 
 void loop() { //main loop function
     while (Serial.available() > 0) { //while there still is serial left to read then read it and adjust stuff
+      
         digitalWrite(LED_BUILTIN, HIGH); //turn on the builtin led to show that there is serial being read
+        
         reading = Serial.read(); //read the serial
+        
         if (reading == SERVO_DIRECTION_ID_UP) { //if the reading matches the id of the up command then pitch the servo up
             YRotationServoPosition += SERVO_ROTATION_STEP;
         } else if (reading == SERVO_DIRECTION_ID_DOWN) { //if the reading matches the id of the down command then pitch the servo down
@@ -37,8 +42,23 @@ void loop() { //main loop function
         } else if (reading == SERVO_DIRECTION_ID_RIGHT) { //if the reading matches the id of the right command then yaw the servo right
             XRotationServoPosition += SERVO_ROTATION_STEP;
         }
+        
+        if (XRotationServoPosition > SERVO_MAX_ROTATION) { //if the yaw servo is over the max bounds then set it to the max rotation
+            XRotationServoPosition = SERVO_MAX_ROTATION;
+        } else if (XRotationServoPosition < SERVO_MIN_ROTATION) { //if the yaw servo is under the minimum bounds then set it to the minimum rotation
+            XRotationServoPosition = SERVO_MIN_ROTATION;
+        }
+
+        if (YRotationServoPosition > SERVO_MAX_ROTATION) { //if the pitch servo is over the max bounds then set it to the max rotation
+            YRotationServoPosition = SERVO_MAX_ROTATION;
+        } else if (YRotationServoPosition < SERVO_MIN_ROTATION) { //if the pitch servo is under the minimum bounds then set it to the minimum rotation
+            YRotationServoPosition = SERVO_MIN_ROTATION;
+        }
+        
         SERVO_X.write(XRotationServoPosition); //tell the servo to move to the position its supposed to be at
         SERVO_Y.write(YRotationServoPosition); //tell the servo to move to the position its supposed to be at
+        
+        delay(20);
     }
     digitalWrite(LED_BUILTIN, LOW); //turn off the builtin led to show that there is no serial being read
 }
